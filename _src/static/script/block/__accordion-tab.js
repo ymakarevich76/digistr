05b23367a2ddb1b43ff0ed087f4e544f906241f3
Юля <ymakarevich76@gmail.com>
2022-05-04 16:55:49 +0300

    Add single seo page

--- _src/static/script/block/__accordion-tab.js
+++ _src/static/script/block/__accordion-tab.js
@@ -1,10 +1,10 @@
-if (document.querySelector('.accordion__tab')) {
-  const accordionTabsContent = document.querySelectorAll('.accordion__tab-content');
+if (document.querySelector('.store-accordion__tab')) {
+  const accordionTabsContent = document.querySelectorAll('.store-accordion__tab-content');
 
   const setAccordionTabTop = () => {
 
     accordionTabsContent.forEach(accordionTabContent => {
-      const accordionTabContentParent = accordionTabContent.closest('.accordion__tab'),
+      const accordionTabContentParent = accordionTabContent.closest('.store-accordion__tab'),
         height = window.getComputedStyle(accordionTabContentParent).height;
 
 
@@ -16,4 +16,18 @@ if (document.querySelector('.accordion__tab')) {
   setAccordionTabTop();
   window.addEventListener('resize', setAccordionTabTop);
 
+}
+
+
+if (document.querySelector('.accordion')) {
+  const accordionBlocks = document.querySelectorAll('.accordion__block');
+
+  accordionBlocks.forEach(item => {
+    item.lastElementChild.classList.add('hide');
+
+    item.firstElementChild.addEventListener('click', (e) => {
+      e.target.nextElementSibling.classList.toggle('hide');
+      e.target.classList.toggle('active');
+    })
+  })
 }
